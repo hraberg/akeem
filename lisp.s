78e550df9351798f59caf5f87e769ec0bc545f60
Håkan Råberg <hakan.raberg@gmail.com>
2016-02-23 18:13:11 +0000

    Group global exports.

--- lisp.s
+++ lisp.s
@@ -483,8 +483,12 @@ plus_int_int:
         box_int_internal %eax
         ret
 
-        .globl allocate_code, cons, car, cdr, length, display, newline, box_int, box_string, unbox
+        .globl cons, car, cdr, length
+        .globl display, newline
         .globl is_eq, is_eq_v, is_string, is_boolean, is_symbol, is_null, is_exact, is_inexact, is_integer, is_number, is_pair, is_vector
         .globl make_vector, vector_length, vector_ref, vector_set
-        .globl make_string, string_length, string_ref, string_set, string_to_number, string_to_symbol, symbol_to_string
-        .globl int_format, double_format, neg, plus, init_runtime, true_string, false_string, set, lookup_global_symbol
+        .globl make_string, string_length, string_ref, string_set, string_to_number, string_to_symbol
+        .globl neg, plus
+        .globl symbol_to_string, set, lookup_global_symbol
+        .globl init_runtime, allocate_code
+        .globl int_format, double_format, true_string, false_string, box_int, box_string, unbox
