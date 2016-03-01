d69d2ff5663ecd5971b59e2f375603405a1b6b08
Håkan Råberg <hakan.raberg@gmail.com>
2016-03-01 22:36:29 +0000

    Refactor REPL.

--- repl.s
+++ repl.s
@@ -6,25 +6,20 @@
 
 main:
         prologue
-        call_fn init_runtime, %rsp
 
-        call_fn box_string, $welcome_message
-        call_fn display, %rax
+        call_fn init_runtime, %rsp
+        intern_string welcome_message, "Welcome to Akeem Scheme."
+        intern_string prompt, "> "
+
+        call_fn display, welcome_message
         call_fn newline
 
-        call_fn box_string, $prompt
-        mov     %rax, %rbx
-1:      call_fn display, %rbx
+1:      call_fn display, prompt
 
         call_fn read
         call_fn write, %rax
         call_fn newline
+
         jmp     1b
 
 2:      return  $0
-
-        .data
-welcome_message:
-        .string "Welcome to Akeem Scheme."
-prompt:
-        .string "> "
