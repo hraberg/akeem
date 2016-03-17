3342a101825b92d38a806323f38df08db8b9d600
Håkan Råberg <hakan.raberg@gmail.com>
2016-03-17 13:29:45 +0000

    Refactoring Scheme fns to use do.

--- r5rs.scm
+++ r5rs.scm
@@ -178,9 +178,9 @@
       (and (pair? obj) (list? (cdr obj)))))
 
 (define (list-tail list k)
-  (if (zero? k)
-      list
-      (list-tail (cdr list) (- k 1))))
+  (do ((list list (cdr list))
+       (k k (- k 1)))
+      ((zero? k) list)))
 
 (define (list-ref list k)
   (car (list-tail list k)))
@@ -249,102 +249,58 @@
 
 ;;; 6.3.5. Strings
 
-(define (string->list-aux string list idx)
-  (if (negative? idx)
-      list
-      (string->list-aux
-       string (cons (string-ref string idx) list) (- idx 1))))
-
 (define (string->list string)
-  (string->list-aux
-   string '() (- (string-length string) 1)))
-
-(define (substring-aux string copy idx start end)
-  (if (= idx end)
-      copy
-      (substring-aux
-       string
-       (begin
-         (string-set! copy (- idx start) (string-ref string idx))
-         copy)
-       (+ idx 1)
-       start
-       end)))
+  (do ((list '() (cons (string-ref string idx) list))
+       (idx (- (string-length string) 1) (- idx 1)))
+      ((negative? idx) list)))
 
 (define (substring string start end)
-  (substring-aux
-   string (make-string (- end start)) start start end))
+  (do ((copy (make-string (- end start)))
+       (idx start (+ idx 1)))
+      ((= idx end) copy)
+    (string-set! copy (- idx start) (string-ref string idx))))
 
 (define (string-append string1 string2)
   (list->string (append (string->list string1) (string->list string2))))
 
-(define (list->string-aux string list idx)
-  (if (null? list)
-      string
-      (list->string-aux
-       (begin
-         (string-set! string idx (car list))
-         string)
-       (cdr list)
-       (+ 1 idx))))
-
 (define (list->string list)
-  (list->string-aux
-   (make-string (length list)) list 0))
+  (do ((list list (cdr list))
+       (string (make-string (length list)))
+       (idx 0 (+ idx 1)))
+      ((null? list) string)
+    (string-set! string idx (car list))))
 
 (define (string-copy string)
   (list->string (string->list string)))
 
-(define (string-fill!-aux string fill idx)
-  (if (negative? idx)
-      string
-      (string-fill!-aux
-       (begin
-         (string-set! string idx fill)
-         string)
-       fill
-       (- idx 1))))
-
 (define (string-fill! string fill)
-  (string-fill!-aux
-   string fill (- (string-length string) 1)))
+  (do ((idx (- (string-length string) 1) (- idx 1)))
+      ((negative? idx) string)
+    (string-set! string idx fill)))
 
 ;;; 6.3.6. Vectors
 
-(define (vector->list-aux vector list idx)
-  (if (negative? idx)
-      list
-      (vector->list-aux
-       vector (cons (vector-ref vector idx) list) (- idx 1))))
-
 (define (vector->list vector)
-  (vector->list-aux
-   vector '() (- (vector-length vector) 1)))
-
-(define (vector-fill!-aux vector fill idx)
-  (if (negative? idx)
-      vector
-      (vector-fill!-aux
-       (begin
-         (vector-set! vector idx fill)
-         vector)
-       fill
-       (- idx 1))))
+  (do ((list '() (cons (vector-ref vector idx) list))
+       (idx (- (vector-length vector) 1) (- idx 1)))
+      ((negative? idx) list)))
 
 (define (vector-fill! vector fill)
-  (vector-fill!-aux
-   vector fill (- (vector-length vector) 1)))
+  (do ((idx (- (vector-length vector) 1) (- idx 1)))
+      ((negative? idx) vector)
+    (vector-set! vector idx fill)))
 
 ;;; 6.4. Control features
 
 (define (map proc list)
-  (if (null? list) '()
-      (cons (proc (car list)) (map proc (cdr list)))))
+  (do ((list list (cdr list))
+       (acc '() (cons (proc (car list)) acc)))
+      ((null? list) (reverse acc))))
 
 (define (for-each proc list)
-  (if (null? list) '()
-      (begin (proc (car list))
-             (for-each proc (cdr list)))))
+  (do ((list list (cdr list)))
+      ((null? list) acc)
+    (proc (car list))))
 
 (define force
   (lambda (object)
