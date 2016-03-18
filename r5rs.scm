29c84095da5a5e544a3e41ba6e13a7b554e1ba3f
Håkan Råberg <hakan.raberg@gmail.com>
2016-03-18 15:50:48 +0000

    Adding commented out versions of the basic derived expression syntax-rules + a quasiquote syntax-rules from comp.lang.scheme.

--- r5rs.scm
+++ r5rs.scm
@@ -4,6 +4,69 @@
   (lambda (form)
     (cons 'lambda (cdr form))))
 
+;;; 7.3. Derived expression types
+
+;; (define-syntax cond
+;;   (syntax-rules (else =>)
+;;     ((cond (else result1 result2 ...))
+;;      (begin result1 result2 ...))
+;;     ((cond (test => result))
+;;      (let ((temp test))
+;;        (if temp (result temp))))
+;;     ((cond (test => result) clause1 clause2 ...)
+;;      (let ((temp test))
+;;        (if temp
+;;            (result temp)
+;;            (cond clause1 clause2 ...))))
+;;     ((cond (test)) test)
+;;     ((cond (test) clause1 clause2 ...)
+;;      (let ((temp test))
+;;        (if temp
+;;            temp
+;;            (cond clause1 clause2 ...))))
+;;     ((cond (test result1 result2 ...))
+;;      (if test (begin result1 result2 ...)))
+;;     ((cond (test result1 result2 ...)
+;;            clause1 clause2 ...)
+;;      (if test
+;;          (begin result1 result2 ...)
+;;          (cond clause1 clause2 ...)))))
+
+;; (define-syntax case
+;;   (syntax-rules (else)
+;;     ((case (key ...)
+;;        clauses ...)
+;;      (let ((atom-key (key ...)))
+;;        (case atom-key clauses ...)))
+;;     ((case key
+;;        (else result1 result2 ...))
+;;      (begin result1 result2 ...))
+;;     ((case key
+;;        ((atoms ...) result1 result2 ...))
+;;      (if (memv key ’(atoms ...))
+;;          (begin result1 result2 ...)))
+;;     ((case key
+;;        ((atoms ...) result1 result2 ...)
+;;        clause clauses ...)
+;;      (if (memv key ’(atoms ...))
+;;          (begin result1 result2 ...)
+;;          (case key clause clauses ...)))))
+
+;; (define-syntax and
+;;   (syntax-rules ()
+;;     ((and) #t)
+;;     ((and test) test)
+;;     ((and test1 test2 ...)
+;;      (if test1 (and test2 ...) #f))))
+
+;; (define-syntax or
+;;   (syntax-rules ()
+;;     ((or) #f)
+;;     ((or test) test)
+;;     ((or test1 test2 ...)
+;;      (let ((x test1))
+;;        (if x x (or test2 ...))))))
+
 ;;; 4.2.6. Quasiquotation
 
 ;; Based on https://github.com/mishoo/SLip/blob/master/lisp/compiler.lisp#L25
@@ -24,6 +87,34 @@
 (define-syntax quasiquote
   (lambda (qq-template)
     (qq (cadr qq-template))))
+
+;; See https://groups.google.com/d/msg/comp.lang.scheme/koHP9xzPF3Y/4Ffi9hq9bqsJ
+;; (define-syntax quasiquote
+;;   (syntax-rules (quasiquote unquote unquote-splicing quote)
+;;     ((quasiquote a)
+;;      (quasiquote "zero" a))
+;;     ((quasiquote l (quote a))
+;;      (cons 'quote (cons (quasiquote l a) '())))
+;;     ((quasiquote "zero" (unquote a))
+;;      a)
+;;     ((quasiquote "zero" b . (unquote a))
+;;      (cons b a))
+;;     ((quasiquote ("succ" . l) (unquote a))
+;;      (cons 'unquote (cons (quasiquote l a) '())))
+;;     ((quasiquote l (unquote-splicing a))
+;;      (error "unquote-splicing"))
+;;     ((quasiquote "zero" ((unquote-splicing a) . r))
+;;      (append a (quasiquote "zero" r)))
+;;     ((quasiquote ("succ" . l) ((unquote-splicing a) . r))
+;;      (cons (cons 'unquote-splicing (cons (quasiquote l a) '())) (quasiquote ("succ" . l) r)))
+;;     ((quasiquote l (quasiquote a))
+;;      (cons 'quasiquote (cons (quasiquote ("succ" . l) a) '())))
+;;     ((quasiquote l (r . r1))
+;;      (cons (quasiquote l r) (quasiquote l r1)))
+;;     ((quasiquote l #(r ...))
+;;      (list->vector (quasiquote l (r ...))))
+;;     ((quasiquote l a)
+;;      (quote a))))
 
 ;;; 6.1. Equivalence predicates
 
